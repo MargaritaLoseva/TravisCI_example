002ba35df6739c62a6c57ab7276f4425819eab02
Margarita <margarita.loseva@devexpress.com>
2016-09-08 15:32:57 +0300

    8

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+import connect from 'connect';
+import serveStatic from 'serve-static';
 
 connect().use(serveStatic(__dirname)).listen(9090, () => {
     console.log('Server running on 9090...');
